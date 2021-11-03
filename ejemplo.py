68e94466ea7557be263e344100d8dd7601d6eb5c
[Daniel Díaz] <[daniel.diaz-r@hotmail.com]>
2021-11-02 19:16:23 -0500

    version 1.1

--- ejemplo.py
+++ ejemplo.py
@@ -1,3 +1,4 @@
+# coding= UTF-8
 from flask import Flask, render_template, request
 
 
@@ -12,6 +13,8 @@ def datos():
     user = {'nombre' : 'david'}
     return render_template('datos.html', title = 'Titulo personalizado', user = user)
 
+
+
 @app.route('/validar', methods = ["POST"])
 def validar():
     if request.method == "POST":
@@ -19,9 +22,26 @@ def validar():
         password = request.form["password"]
         
         resultado = verificar(usuario, password)
+        """ if resultado == True:
+            return "Bienvenido" """
+    return render_template("menu.html", title = "Sistema DABM")        
 
-def verificar(usuario, resultado):
-    pass        
+
+def verificar(usuario, password):
+    pass
+    return True
+"""     file = open("C:/Users/USUARIO/OneDrive/Documents/DABM/Codigos/flask/bd/users.csv", "r")
+    datosArchivo = file.readlines()        
+    file.close()
+
+    for dato in datosArchivo:
+        usuarioR, passwordR = dato.split(";")
+        if usuario == usuarioR and password == passwordR:
+            return True
+        else:
+            return False     """
+    
+
 
 
 
